826acfb3e13d7d789c07b7061fa0eebed4b024f7
Nivi250998 <nivedsundaram24@gmail.com>
2024-11-10 23:21:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,13 @@ if ingredients_list:
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+if fruityvice_response.status_code == 200:
+    st.text(fruityvice_response.json())
+else:
+    st.text(f"Error: {fruityvice_response.status_code}")
+
+
+#st.text(fruityvice_response.json())
 #fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=true)   
      
 
